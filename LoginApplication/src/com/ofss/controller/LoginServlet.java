f1c58ea9175ccc5aaf3b6cfac120402cfefb824d
Guru <gurumurthy.ramamurthy@gmail.com>
2023-04-20 05:27:21 +0530

    test2

--- LoginApplication/src/com/ofss/controller/LoginServlet.java
+++ LoginApplication/src/com/ofss/controller/LoginServlet.java
@@ -29,7 +29,7 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		System.out.println("Just a line ");
+		System.out.println("Just a line s ");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
